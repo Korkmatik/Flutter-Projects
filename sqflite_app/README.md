23590e85d493412cb67a25544c715b0eb6970f75
Korkmatik <git.c8tmx@aleeas.com>
2022-04-24 02:36:12 +0200

    Add sqflite app

---
+++ sqflite_app/README.md
@@ -0,0 +1,14 @@
+# sqflite_app
+
+A simple note taking app which uses the sqflite package to persist data. Made with [this tutorial](https://youtu.be/UpKrhZ0Hppk).
+
+# Screenshots
+
+## Main Page
+![](./screenshots/main_page.png)
+
+## Add Note
+![](./screenshots/add_note.png)
+
+## Edit Note
+![](./screenshots/edit_note.png)
---
+++ sqflite_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_app/page/notes_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Notes App',
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        )
+      ),
+      home: NotesPage(),
+    );
+  }
+}
+
